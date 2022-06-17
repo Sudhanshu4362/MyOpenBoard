a985eaca11cab715f9d4b7ef8e7726e0a37db555
Sudhanshu Mishra <mishrasudhansh855@gmail.com>
2022-06-17 13:25:29 +0530

    final

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const socket = require("socket.io");
 const app = express();//initialize
 app.use(express.static("FrontEnd"));
 
-let port = 5000;
+let port = process.env.PORT || 5000;
 let server = app.listen(port, () => {
     console.log("Listening to port" + port);
 })
